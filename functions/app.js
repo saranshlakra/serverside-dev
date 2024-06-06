eef29248eeedad0cc297cb7b5076e03f6de33a1b
Saransh Lakra <saranshlakra4@gmail.com>
2024-06-06 11:39:57 -0400

    update

--- functions/app.js
+++ functions/app.js
@@ -8,10 +8,12 @@ const router = express.Router();
 
 app.use(cors());
 
+
+
 router.get('/api/waldom/:partNumber', async (req, res) => {
     try {
         const { partNumber } = req.params;
-        const response = await axios.get(`https://www.waldom.com/api/v1/${process.env.WALDOM_API_KEY}/ProductSearch/${partNumber}/0/1/1`, {
+        const response = await axios.get(`https://www.waldom.com/api/v1/7294de28-6ee1-40f1-ad36-36123982199c/ProductSearch/${partNumber}/0/1/1`, {
             headers: {
                 'Authorization': `Bearer ${process.env.WALDOM_API_KEY}`
             }
